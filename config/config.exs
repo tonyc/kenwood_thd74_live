1a20fb5d4f1cc4370de2fb8de44c4584a0ccf9b3
Tony Collen <tony@halogenlabs.com>
2019-11-10 15:12:24 -0600

    Set up LiveView

--- config/config.exs
+++ config/config.exs
@@ -15,7 +15,8 @@ config :liveview_demo, LiveviewDemoWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "C2g2G8j9z4J0QXUlWHBlPHK9FlFXK9AqauDgrc17uqEm+hYNPov+uwpJzpoUuFH1",
   render_errors: [view: LiveviewDemoWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: LiveviewDemo.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: LiveviewDemo.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [signing_salt: "x4UXuVA0zYVmN/5KbqFo+EDiW8r4NL9Y"]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,6 +25,9 @@ config :logger, :console,
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
+
+config :phoenix,
+  template_engines: [leex: Phoenix.LiveView.Engine]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- lib/liveview_demo_web.ex
+++ lib/liveview_demo_web.ex
@@ -23,6 +23,7 @@ defmodule LiveviewDemoWeb do
 
       import Plug.Conn
       import LiveviewDemoWeb.Gettext
+      import Phoenix.LiveView.Controller
       alias LiveviewDemoWeb.Router.Helpers, as: Routes
     end
   end
@@ -42,6 +43,17 @@ defmodule LiveviewDemoWeb do
       import LiveviewDemoWeb.ErrorHelpers
       import LiveviewDemoWeb.Gettext
       alias LiveviewDemoWeb.Router.Helpers, as: Routes
+
+      import Phoenix.LiveView,
+        only: [
+          live_render: 2,
+          live_render: 3,
+          live_link: 1,
+          live_link: 2,
+          live_component: 2,
+          live_component: 3,
+          live_component: 4
+        ]
     end
   end
 
@@ -50,6 +62,7 @@ defmodule LiveviewDemoWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
+      import Phoenix.LiveView.Router
     end
   end
 
