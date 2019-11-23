4c682c1cc8debb74b36ffdef0af9fe145ffb4634
Tony Collen <tony@halogenlabs.com>
2019-11-22 20:32:55 -0600

    Rework radio info parsing a little

--- lib/kenwood_d74/radio_info.ex
+++ lib/kenwood_d74/radio_info.ex
@@ -5,15 +5,26 @@ defmodule KenwoodD74.RadioInfo do
   defstruct cmd: "", args: []
 
   def parse(raw_msg) do
-    [command, args] =
+    parts =
       raw_msg
       |> String.split(" ")
 
-    Logger.info("RadioInfo.parse: args: #{inspect command}")
-    split_args =
-      args
-      |> String.split(",")
+    case parts do
+      [cmd] ->
+        %__MODULE__{cmd: cmd, args: []}
 
-    %__MODULE__{cmd: command, args: split_args}
+      [cmd | args] ->
+        Logger.debug("parse: cmd: #{inspect(cmd)}, args: #{inspect(args)}")
+
+        exploded_args =
+          args
+          |> List.first()
+          |> String.split(",")
+
+        %__MODULE__{cmd: cmd, args: exploded_args}
+
+      _ ->
+        %__MODULE__{cmd: "", args: []}
+    end
   end
 end
